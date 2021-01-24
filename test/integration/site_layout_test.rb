dbba07b59b1d24d7eac474447d2a461fccd320bf
james26k <cb26.hayashi@gmail.com>
2021-01-24 16:27:48 +0900

    add: testでもApplicationHelperで定義したヘルパーを利用できるよう機能追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get help_path
+    assert_select "title", full_title("Help")
+    get about_path
+    assert_select "title", full_title("About")
+    get contact_path
+    assert_select "title", full_title("Contact")
   end
 end
